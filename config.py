6ec4c81728e303677d98b918f155bcd64c4fa6bd
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2026-03-22 23:46:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ id_pattern = re.compile(r'^.\d+$')
 
 API_ID = os.environ.get("API_ID", "10811400")
 API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6626666215:AAFSI_ZRp6aoTy9boDgxkrd_2PjyT4myeGg")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6626666215:AAEBK2X3zVVCvav8unKojDVGC0xQ_rCyzl8")
 ADMIN = [6469754522]
 DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://HARSHA24:HARSHA24@cluster0.sxaj8up.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "GroupScannerBot")
