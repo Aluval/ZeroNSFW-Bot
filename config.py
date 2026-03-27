325f5950cfd9e505e2a22618384746d664aa66d1
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2026-03-27 23:24:22 +0530

    ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
    Replaced API_ID environment variable with a hardcoded value.

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 
 id_pattern = re.compile(r'^.\d+$')
 
-API_ID = os.environ.get("API_ID", "10811400")
+API_ID = 10811400  
 API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6626666215:AAEBK2X3zVVCvav8unKojDVGC0xQ_rCyzl8")
 ADMIN = [6469754522]
